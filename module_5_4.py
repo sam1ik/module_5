2892e9ea6f972b0255a0aee0a2da31f73ce2b1ea
sam1ik <samvelik277@gmail.com>
2025-01-20 20:31:31 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,69 @@
+# Задача 'История строительства'
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name_house, num_of_floors):
+        self.name = name_house
+        self.number_of_floors = int(num_of_floors)
+
+    def go_to(self, new_floor):
+        if new_floor not in range(1, self.number_of_floors + 1):
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, value):
+        return House(self.name, self.number_of_floors + value)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
